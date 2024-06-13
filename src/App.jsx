6b3796d130454093a154da01bfe976fdac8d396a
nyagaderrick <nyagad@icloud.com>
2024-06-13 21:13:50 +0300

    removing selecting page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './home/home';
 import Booking from './booking/booking';
-import Selecting from './selecting/selecting';
+
 
 function App() {
   return (
@@ -9,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/booking" element={<Booking />} />
-        <Route path="/selecting" element={<Selecting />} />
       </Routes>
     </Router>
   );
