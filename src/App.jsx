e654356b133b0a7fdc522b980b0a654292bc0d7e
nyagaderrick <nyagad@icloud.com>
2024-06-13 21:27:51 +0300

    adding selecting page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './home/home';
 import Booking from './booking/booking';
-
+import Selecting from './selecting/selecting'; // Import the Selecting component
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/booking" element={<Booking />} />
+        <Route path="/selecting" element={<Selecting />} /> {/* Add a new Route for the Selecting page */}
       </Routes>
     </Router>
   );
---
+++ src/selecting/selecting.jsx
@@ -0,0 +1,11 @@
+// File: src/selecting/Selecting.jsx
+
+function Selecting() {
+  return (
+    <div>
+      <h1>Selecting Page</h1>
+    </div>
+  );
+}
+
+export default Selecting;
